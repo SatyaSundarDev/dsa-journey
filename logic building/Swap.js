400498cc03bf90c01dd461655ceab1ce242b4af7
Satya Sundar <satyasundar439@gmail.com>
2026-01-03 22:17:12 +0530

    Solved sum of squeares and Swapping of numbers

---
+++ logic building/Swap.js
@@ -0,0 +1,9 @@
+let a = 10, b = 15;
+console.log("a = "+a + " " + "b = " + b);
+
+let temp = a;
+a= b;
+b= temp;
+
+console.log("After swaping: a = "+a + " b = "+b);
+
---
+++ logic building/sumOfSquares.js
@@ -0,0 +1,6 @@
+function sumOfSquares(n){
+    return (n*(n+1)*(2*n+1))/6;
+}
+
+n=10;
+console.log(sumOfSquares(n));
